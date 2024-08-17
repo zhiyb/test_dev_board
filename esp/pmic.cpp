9794c11e57956ccad4e0590390757ba97af4e9a5
Norman Zhi <normanzyb@gmail.com>
2024-08-17 17:10:23 +0100

    esp: Schedule power-on every hour

--- esp/pmic.cpp
+++ esp/pmic.cpp
@@ -9,36 +9,41 @@
 typedef enum {
     I2cRegId = 0,
     I2cRegState,
-    I2cRegAdcTempL,
-    I2cRegAdcTempH,
-    I2cRegAdcVbgL,
-    I2cRegAdcVbgH,
+    I2cRegBootMode,
+    I2cRegAdcTemp0,
+    I2cRegAdcTemp1,
+    I2cRegAdcVbg0,
+    I2cRegAdcVbg1,
+    I2cRegEspTimeoutSec0,
+    I2cRegEspTimeoutSec1,
+    I2cRegPicoTimeoutSec0,
+    I2cRegPicoTimeoutSec1,
+    I2cRegEspScheduleSec0,
+    I2cRegEspScheduleSec1,
+    I2cRegEspScheduleSec2,
+    I2cRegEspScheduleSec3,
+    I2cRegPicoScheduleSec0,
+    I2cRegPicoScheduleSec1,
+    I2cRegPicoScheduleSec2,
+    I2cRegPicoScheduleSec3,
 } i2c_reg_t;
 
 static const unsigned int gpio_sda = 4;
 static const unsigned int gpio_scl = 5;
 
-static void pmic_write(uint8_t reg, uint8_t val)
+static void pmic_write_multi(uint8_t reg, const uint8_t *buf, uint8_t len)
 {
     Wire.beginTransmission(I2C_ADDR);
     Wire.write(reg);
-    Wire.write(val);
+    for (uint8_t i = 0; i < len; i++)
+        Wire.write(buf[i]);
     Wire.endTransmission(true);
     delayMicroseconds(10);
 }
 
-static uint8_t pmic_read(uint8_t reg)
+static void pmic_write(uint8_t reg, uint8_t val)
 {
-    Wire.beginTransmission(I2C_ADDR);
-    Wire.write(reg);
-    Wire.endTransmission(false);
-
-    Wire.requestFrom(I2C_ADDR, 1u, true);
-    uint8_t v = 0;
-    if (Wire.available())
-        v = Wire.read();
-    delayMicroseconds(10);
-    return v;
+    pmic_write_multi(reg, &val, 1);
 }
 
 static uint8_t pmic_read_multi(uint8_t reg, uint8_t *buf, uint8_t len)
@@ -55,6 +60,13 @@ static uint8_t pmic_read_multi(uint8_t reg, uint8_t *buf, uint8_t len)
     }
     delayMicroseconds(10);
     return i;
+}
+
+static uint8_t pmic_read(uint8_t reg)
+{
+    uint8_t v = 0;
+    pmic_read_multi(reg, &v, 1);
+    return v;
 }
 
 void pmic_init(void)
@@ -88,7 +100,7 @@ void pmic_update(void)
 {
     uint8_t state = pmic_read(I2cRegState);
     uint16_t adc_val[2] = {0};
-    pmic_read_multi(I2cRegAdcTempL, (uint8_t *)&adc_val[0], 4);
+    pmic_read_multi(I2cRegAdcTemp0, (uint8_t *)&adc_val[0], 4);
     // Serial.printf("state=0x%02x adc_0=0x%04x adc_1=0x%04x\n",
     //     (int)state, (int)adc_val[0], (int)adc_val[1]);
 
@@ -107,4 +119,7 @@ void pmic_update(void)
     resp = http_post(url, nullptr, &data[0], len);
 
     // TODO Get next wakeup time
+    // Schedule next wakeup after 1 hour
+    uint32_t schedule_sec = 60 * 60;
+    pmic_write_multi(I2cRegEspScheduleSec0, (uint8_t *)&schedule_sec, 4);
 }
